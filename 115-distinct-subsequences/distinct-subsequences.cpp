a2e78d3f432ed94c11b28b20ca004af1e6a75833
Anushka Darekar <anushka.anushkadarekar@gmail.com>
2026-09-06 23:11:20 +0530

    Time: 25 ms (73.87%) | Memory: 27.1 MB (52.57%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -5,24 +5,16 @@ public:
         int n = t.size();
 
         const int LIMIT = INT_MAX;
-
         vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-
-        // Empty t can be formed in exactly 1 way
         dp[0][0] = 1;
 
         for (int i = 1; i <= m; i++) {
             dp[i][0] = 1;
 
             for (int j = 1; j <= n; j++) {
-
-                // Don't use s[i-1]
                 dp[i][j] = dp[i - 1][j];
-
-                // Use s[i-1] if characters match
                 if (s[i - 1] == t[j - 1]) {
 
-                    // Prevent integer overflow
                     if (dp[i][j] > LIMIT - dp[i - 1][j - 1]) {
                         dp[i][j] = LIMIT;
                     } else {
